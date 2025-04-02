49492123de7a4042c72b0c3a85bf8bea0ca463be
Marco Lanconelli <marko_lan@hotmail.it>
2025-04-02 19:51:51 +0200

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import icon from 'astro-icon';
 // https://astro.build/config
 export default defineConfig({
   site: "https://mistahuman.github.io/portfolio-davidesigillo/",
-  base: process.env.NODE_ENV === "production" ? "/portfolio-davidesigillo/" : "/",
+  base: process.env.NODE_ENV === "production" ? "/portfolio-davidesigillo/" : "/local/",
   output: "static",
   integrations: [icon()]
 });
